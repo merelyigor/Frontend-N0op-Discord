cff4431ae8232c206b8baf42974602b6336f4313
nooppie <kalagen65@gmail.com>
2025-05-19 23:56:14 +0300

    HERO LOH

---
+++ noopOG/src/js/parts/main-page.js
@@ -0,0 +1,35 @@
+export function formSendCheckCheckbox() {
+
+    document.querySelector(".hero__right form")
+        .addEventListener('submit', function (e) {
+
+
+            const checkbox = document.querySelector("#checkbox-1");
+            const label = document.querySelector(".custom-checkbox label");
+            const loginInput = document.querySelector("#login");
+            const pass = document.querySelector("#password");
+            const loginLine = document.querySelector(".login_line");
+            const passLine = document.querySelector(".password_line");
+
+
+            if (loginInput.value === '') {
+                e.preventDefault();
+                loginLine.classList.add("error");
+            }
+            if (pass.value === '') {
+                e.preventDefault();
+                passLine.classList.add("error");
+
+            }
+
+            if (!checkbox.checked) {
+                e.preventDefault();
+                label.classList.add("error");
+            }
+            setTimeout(() => {
+                loginLine.classList.remove("error");
+                passLine.classList.remove("error");
+                label.classList.remove("error");
+            }, 2000)
+        })
+}
