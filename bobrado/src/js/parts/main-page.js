9cd5488f09cdc65077170ae6d440d25ca5a65d21
Vitalititi <plotnitskyvitaliy2345@gmail.com>
2025-05-19 23:56:12 +0300

    har4lox

---
+++ bobrado/src/js/parts/main-page.js
@@ -0,0 +1,34 @@
+export function formSendCheckCheckbox() {
+    document.querySelector('.hero__right form')
+        .addEventListener('submit', function (e) {
+            const checkbox = document.querySelector('#checkbox-1');
+            const label = document.querySelector('.custom-checkbox label');
+            const loginInput = document.querySelector('#login')
+            const passInput = document.querySelector('#password')
+            const loginLine = document.querySelector('.login_line')
+            const passLine = document.querySelector('.password_line')
+
+
+
+            if (loginInput.value === '') {
+                e.preventDefault();
+                loginLine.classList.add('error');
+            }
+
+            if (passInput.value === '') {
+                e.preventDefault();
+                passLine.classList.add('error');
+            }
+
+
+            if (!checkbox.checked) {
+                e.preventDefault();
+                label.classList.add('error');
+            }
+            setTimeout(() => {
+                label.classList.remove('error');
+                loginLine.classList.remove('error');
+                passLine.classList.remove('error');
+            }, 2000)
+        })
+}
