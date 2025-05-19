cea8b072917ec41cff780bff48d0c9236c360dad
Merely_igor_homePC <mogila1708@gmail.com>
2025-05-19 23:55:53 +0300

    v32

--- merelyigor/src/js/main.js
+++ merelyigor/src/js/main.js
@@ -3,9 +3,11 @@ console.log('+++++++==== START JS PROJECT ====+++++');
 import "../style/main.scss";
 
 import {headerPopup} from "./parts/header";
+import {formSendCheckCheckbox} from "./parts/main-page";
 
 document.addEventListener('DOMContentLoaded', function () {
     headerPopup();
+    formSendCheckCheckbox();
 })
 
 
