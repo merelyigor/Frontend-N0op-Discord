6bb72a132f4507ffb85c27555a11893f98dbbb40
Igor Eremenko <mogila1708@gmail.com>
2025-06-09 02:09:54 +0300

    Fix typo in romashka form validation

--- romashka/src/js/main.js
+++ romashka/src/js/main.js
@@ -2,11 +2,11 @@ console.log('+++++++++++ START JS PROJECT +++++++++++');
 
 import "../style/main.scss";
 import {headerPopup} from "./parts/header";
-import {fromSendCheckCheckbox} from "./parts/main-page";
+import {formSendCheckCheckbox} from "./parts/main-page";
 
 document.addEventListener("DOMContentLoaded", function () {
     headerPopup();
-    fromSendCheckCheckbox();
+    formSendCheckCheckbox();
 })
 
 
